e251d8b18c92d0efd7fec3c9fc5b3c26c91658a1
eggmam <1257657100@qq.com>
2021-07-12 13:18:50 +0800

    '初始化项目'

---
+++ src/test/java/com/haitao/javawebproject/JavaWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haitao.javawebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
